4152a76bb6bd56af02a32a89178010e8b5bbab2a
Tom Giles <tom.giles@softwire.com>
2025-09-16 15:07:51 +0100

    Refactor getArrivals + handle key not found

--- backend/fetchArrivals.ts
+++ backend/fetchArrivals.ts
@@ -5,14 +5,14 @@ const key = import.meta.env.VITE_API_KEY;
 export default async function getArrivals(id: string){
     let url = `https://api.tfl.gov.uk/StopPoint/${id}/Arrivals`;
    
-    if (key !== ""){
-        url = url + `?=${key}`;
+    if (key){
+        url += `?app_key=${key}`, url;
     }
 
     try{
         const response = await axios.get(url);
         return JSON.stringify(response.data);
     } catch (error){
-        console.error(error);
+        return "500: Internal Server Error"
     }
 }
