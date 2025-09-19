21a1540c76e47035bad6e9dfc5713fcbc0c8eaf7
Tom Giles <tom.giles@softwire.com>
2025-09-19 15:45:47 +0100

    Refactor busboard functionality to dedicated component

---
+++ src/BusBoard.tsx
@@ -0,0 +1,23 @@
+import {Arrivals} from '../components/Arrivals';
+import {NearestArrivals} from '../components/NearestArrivals';
+
+function BusBoard() {
+  return (
+    <>
+        <h1 className="text-3xl font-bold underline text-center text-cyan-600 m-4"
+        >BusBoard</h1>
+        <div className='flex flex-row'>
+            <div className='flex-grow'/>
+            <div className='ml-2 mr-2'>
+              <Arrivals/>
+            </div>
+            <div className='ml-2 mr-2'>
+              <NearestArrivals/>
+            </div>
+          <div className='flex-grow'/>
+        </div>
+    </>
+  )
+}
+
+export default BusBoard
