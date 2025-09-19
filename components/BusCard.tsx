a7b8c013581d7e6025457cd02c7fb8aa53179cab
Tom Giles <tom.giles@softwire.com>
2025-09-19 14:07:08 +0100

    Tailwind css improvements to BusCard

--- components/BusCard.tsx
+++ components/BusCard.tsx
@@ -5,7 +5,7 @@ export function BusCard({busData} : {busData : Bus}){
     let timeInMinues = Math.round(busData.timeToStation / 60);
     return (
     <>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="max-w-sm rounded overflow-hidden shadow-sm mt-3 mb-1 hover:bg-blue-100">
         <p>{busData.lineName} from {busData.destinationName} to {busData.towards} due in {timeInMinues} {timeInMinues === 1 ? "minute" : "minutes"}</p>
       </div>
     </>
