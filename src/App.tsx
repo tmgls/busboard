ea00e5abacb278fee6394c118852fc32a12f741c
Tom Giles <tom.giles@softwire.com>
2025-09-19 14:23:39 +0100

    Centre ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,16 @@ function App() {
     <>
         <h1 className="text-3xl font-bold underline text-center text-cyan-600 m-4"
         >BusBoard</h1>
-        <Arrivals/>
-        <NearestArrivals/>
+        <div className='flex flex-row'>
+            <div className='flex-grow'/>
+            <div className='ml-2 mr-2'>
+              <Arrivals/>
+            </div>
+            <div className='ml-2 mr-2'>
+              <NearestArrivals/>
+            </div>
+          <div className='flex-grow'/>
+        </div>
     </>
   )
 }
