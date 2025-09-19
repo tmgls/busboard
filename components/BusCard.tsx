3f15f542920dfea460603127d05fd6a9c1f39549
Tom Giles <tom.giles@softwire.com>
2025-09-19 15:46:02 +0100

    Add routing functionality

--- components/BusCard.tsx
+++ components/BusCard.tsx
@@ -1,13 +1,16 @@
 import {Bus} from '../backend/fetchArrivals';
+import {Link} from 'react-router-dom';
 
 export function BusCard({busData} : {busData : Bus}){
   if (busData !== null && busData.lineName !== ""){
     let timeInMinues = Math.round(busData.timeToStation / 60);
     return (
     <>
-      <div className="max-w-sm rounded overflow-hidden shadow-sm mt-3 mb-1 hover:bg-blue-100">
-        <p>{busData.lineName} from {busData.destinationName} to {busData.towards} due in {timeInMinues} {timeInMinues === 1 ? "minute" : "minutes"}</p>
-      </div>
+        <Link to="/History">
+          <div className="max-w-sm rounded overflow-hidden shadow-sm mt-3 mb-1 hover:bg-blue-100">
+            <p>{busData.lineName} from {busData.destinationName} to {busData.towards} due in {timeInMinues} {timeInMinues === 1 ? "minute" : "minutes"}</p>
+          </div>
+        </Link>
     </>
   )
   }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route, Link  } from 'react-router-dom'
+import BusBoard from './BusBoard.tsx'
+import History from './History.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<BusBoard />}/>
+      <Route path="/History" element={<History />}/>
+    </Routes>`
+    </BrowserRouter>
   </StrictMode>,
 )
